aaa228423000afc75fc22a842792f0e54eacbe2c
Tandie <ysoyucho@gmail.com>
2025-09-14 12:46:40 +0300

    added changes

--- server/app.py
+++ server/app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    return ''.join(f"{i}\n" for i in range(param))
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return f'Invalid operation: {operation}'
+    return str(result)
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
